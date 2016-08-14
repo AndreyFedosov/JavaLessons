786caf86c20e92c0fbe805d415c2177dea915d2a
AFedosov <andrey.a.fedosov@gmail.com>
2016-08-14 18:27:19 +0300

    first lesson

--- src/src/lesson1/ThirdClass.java
+++ src/src/lesson1/ThirdClass.java
@@ -3,5 +3,6 @@ package lesson1;
 public class ThirdClass {
     public static void main(String[] args) {
         System.out.println("My first class on Java");
+        System.out.println("My first class on Java");   
     }
 }
