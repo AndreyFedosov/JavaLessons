0f1b980244031249c0b3a6a1ab88c85e0c059632
AFedosov <andrey.a.fedosov@gmail.com>
2016-08-14 18:02:06 +0300

    first lesson

---
+++ src/src/lesson1/Fifth.java
@@ -0,0 +1,7 @@
+package lesson1;
+
+public class Fifth {
+    public static void main(String[] args) {
+        System.out.println("My first class on Java");
+    }
+}
---
+++ src/src/lesson1/ThirdClass.java
@@ -0,0 +1,7 @@
+package lesson1;
+
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("My first class on Java");
+    }
+}
