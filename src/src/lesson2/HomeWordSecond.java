d14714bdb3d9dc267a4a3716ea62b108f22fecb5
AFedosov <andrey.a.fedosov@gmail.com>
2016-08-27 23:26:24 +0300

    homework 2.3

--- src/src/lesson2/HomeWordSecond.java
+++ src/src/lesson2/HomeWordSecond.java
@@ -2,10 +2,23 @@ package lesson2;
 
 public class HomeWordSecond {
     public static double COMMISIONINPERCENT = 5;
+    public static double[] BALANCES = {1200, 250, 2000, 500, 3200};
+    public static String[] OWNERNAMES = {"Jane", "Ann", "Jack", "Oww", "Lane"};
 
     public static double withdrawBalance(double balance, double withdrawal) {
 
         return balance - withdrawal - (withdrawal*COMMISIONINPERCENT/100);
+    }
+
+    public static double withdrawBalance(String ownerName, double withdrawal) {
+
+        for (int i = 0; i < OWNERNAMES.length; i++) {
+            if (OWNERNAMES[i].equals(ownerName))
+            {
+                return withdrawBalance(BALANCES[i],withdrawal);
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -13,11 +26,23 @@ public class HomeWordSecond {
         double withdrawal = 10;
         double withdrawBalance = withdrawBalance(balance,withdrawal);
 
+        String ownerName = "Ann";
+        double sumOfWithdrawal = 100;
+
+        //Homework 2.1
         if (withdrawBalance >= 0) {
             System.out.println("Withdrawal: ОК, Commission is: " + (withdrawal*COMMISIONINPERCENT/100) + ", Balance is: " + withdrawBalance);
         } else {
             System.out.println("Withdrawal: NO. Please check your balance. Commission is " + COMMISIONINPERCENT + "% from payment");
+        }
 
+        //Homework 2.2
+
+        withdrawBalance = withdrawBalance(ownerName,sumOfWithdrawal);
+        if (withdrawBalance >= 0) {
+            System.out.println("Withdrawal: ОК, Sum of withdrawal is " + sumOfWithdrawal + ", Balance is: " + withdrawBalance);
+        } else {
+            System.out.println("Withdrawal: NO. Please check your balance. Commission is " + COMMISIONINPERCENT + "% from payment");
         }
 
     }
