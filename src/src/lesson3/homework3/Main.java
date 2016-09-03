8e6783666208a98833daf60cf0bce1d41787fdcb
AFedosov <andrey.a.fedosov@gmail.com>
2016-09-04 00:33:00 +0300

    Module 3 homework 3

--- src/src/lesson3/homework3/Main.java
+++ src/src/lesson3/homework3/Main.java
@@ -1,22 +1,4 @@
 package lesson3.homework3;
-/*
-You need to create following structure of the classes.
-
-Class Course with fields: Date startDate, String name, int hoursDuration, String teacherName.
-Create two constructors with fields startDate, name; and hourseDuration, name, teacherName.
-
-Class Student with fields: String firstName, String lastName, int group, Course[] coursesTaken, int age.
-With 2 constructors firstName, lastName, group; and lastName, coursesTaken.
-
-CollegeStudent that is a child of Student. With additional fields: String collegeName, int rating, long id.
-Create three constructors: two the same as in Student and one with all the fields of the class.
-
-Class SpecialStudent that is child of CollegeStudent. With additional fields long secretKey, String email.
-Create three constructors: two the same as in CollegeStudend and one with secretKey field.
-
-Create getters and setters for all fields and make fields private as OOP principle follows.
-Create 5 objects Course class. Create objects of others classes using every constructor. You must have 13 objects in Solution class.
- */
 
 import java.util.Date;
 
